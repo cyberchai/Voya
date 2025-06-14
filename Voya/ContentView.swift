9ca57f408d1ffeece66d1e7f4dd9da04471740b6
cyberchai <chairachananharder@gmail.com>
2025-06-14 16:20:35 -0400

    Initial Commit

---
+++ Voya/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Voya
+//
+//  Created by Chaira Harder on 6/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Voya/VoyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoyaApp.swift
+//  Voya
+//
+//  Created by Chaira Harder on 6/14/25.
+//
+
+import SwiftUI
+
+@main
+struct VoyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
